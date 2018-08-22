f9c36707436f09a52e222a9105c676e1162f8379
Kamilczysko <kamiwal92@gmail.com>
2018-08-22 21:34:02 +0200

    code cleaning

--- src/RecordAPT.java
+++ src/RecordAPT.java
@@ -2,65 +2,66 @@ import java.util.*;
 
 public class RecordAPT {
 
-
-
-    public Value createRecord(String record){
-        List<String> tableOfRecordItems = getTableOfRecordItems(record);
-        ListIterator<String> iterator = tableOfRecordItems.listIterator();
+    public Value getComplexValueFromString(String record){
+        List<String> listOfRecordStrings = splitRecordToTableOfStrings(record);
+        ListIterator<String> iterator = listOfRecordStrings.listIterator();
         Value apt = new ComplexValue();
-        processListOfParameters(iterator, apt);
+
+        convertRecordToComplexValue(iterator, apt);
         return apt;
     }
 
-    private Value processListOfParameters(ListIterator<String> iterator, Value mainValue){
+    private Value convertRecordToComplexValue(ListIterator<String> iterator, Value mainValue){
 
         while(iterator.hasNext()){
             String next = iterator.next();
 
             if(next.equals("("))
-                mainValue.addParameter(processListOfParameters(iterator, new ComplexValue()));
+                mainValue.addParameter(convertRecordToComplexValue(iterator, new ComplexValue()));
             else if(next.equals(")")) {
                 ((ComplexValue)mainValue).setParametersStack();
                 return mainValue;
             }else if(next.isEmpty())
                 mainValue.addParameter(null);
             else
-                if(isWord(next)) {
-                    if (mainValue.hasWord()) {
-                        mainValue.addParameter(new BasicStringValue(next));
-                    } else
-                        mainValue.setWord(next);
-                } else if (isDecimal(next))
-                    mainValue.addParameter(new BasicDoubleValue(next));
-                else if(isInt(next))
-                    mainValue.addParameter(new BasicIntValue(next));
+                assignAppropriateValues(next, mainValue);
         }
         ((ComplexValue)mainValue).setParametersStack();
         return mainValue;
     }
 
-    private List<String> getTableOfRecordItems(String apt){
+    private void assignAppropriateValues(String next, Value mainValue){
+        if(isWord(next)) {
+            if (mainValue.hasWord())
+                mainValue.addParameter(new BasicStringValue(next));
+            else
+                mainValue.setWord(next);
+        } else if (isDecimal(next))
+            mainValue.addParameter(new BasicDoubleValue(next));
+        else if(isInt(next))
+            mainValue.addParameter(new BasicIntValue(next));
+    }
 
+    private List<String> splitRecordToTableOfStrings(String apt){
         List<String> recordList = new LinkedList<>();
 
         String[] split = apt.split(",|/");
-
         for(String part : split)
-            recordList.addAll(splitRecordPart(part));
+            recordList.addAll(splitOnePieceOfRecord(part));
 
         return recordList;
     }
 
-    private List<String> splitRecordPart(String part){
+    private List<String> splitOnePieceOfRecord(String part){
         String a = part.replaceAll("(\\s+)|(\\$+)","");
         return Arrays.asList(a.split("(?<=\\(|\\))|(?=\\(|\\))"));
     }
 
-    private static boolean isDecimal(String word) {
+    private boolean isDecimal(String word) {
         return word.matches("[-]?[0-9]+[.]+[0-9]+");
     }
 
-    private static boolean isInt(String word) {
+    private boolean isInt(String word) {
         return word.matches("[-]?[0-9]*");
     }
 
@@ -73,12 +74,12 @@ public class RecordAPT {
     public static void main(String[] args) {
         String txt ="TLON,GOFWD/      (CIRCLE/     -5.00000,     65.00000,     50.00000,$\n" +
                 "      15.00000),ON,(LINE/     -5.00000,     65.00000,     50.00000,$\n" +
-                "                              -5.00000, ,50.00000,     50.00000)";
+                "                              -5.00000, ,51.00000,     50.00000)";
 
         RecordAPT r = new RecordAPT();
-        Value record = r.createRecord(txt);
-        String s = drawRecord(((ComplexValue) record), "");
-        System.out.println(record.getWord()+"::\n"+s);
+        Value record = r.getComplexValueFromString(txt);
+        String s = record.getWord()+"::\n"+drawRecord(((ComplexValue) record), "");
+        System.out.println(s);
     }
 
 
@@ -88,7 +89,7 @@ public class RecordAPT {
         List<Value> parameters = record.getParameters();
         for(Value myRecord : parameters){
             if(myRecord instanceof ComplexValue) {
-                output.append(tabs + myRecord.getWord() + " : \n");
+                output.append(tabs + "-"+myRecord.getWord() + " : \n");
                 String s = drawRecord((ComplexValue) myRecord, tabs);
                 output.append(s);
             }else if(myRecord == null) {
